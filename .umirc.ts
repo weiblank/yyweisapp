895489d33e22a64fa2a39c4a53dd7a4f172ac418
weiyayun <weiyayun@waiqin365.com>
2023-06-01 14:20:22 +0800

    修改umi配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,14 @@
+/*
+ * @Author: weiyayun
+ * @Date: 2023-02-21 18:49:10
+ * @Interface:
+ * @Description:
+ */
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'yyweisapp',
   favicon: 'https://img1.gtimg.com/sports/pics/hv1/186/190/1023/66569211.jpg',
   logo: 'https://img1.gtimg.com/sports/pics/hv1/186/190/1023/66569211.jpg',
-  outputPath: 'docs-dist',
+  outputPath: 'docs',
 });
